c3068460e10849403a67cb2f4e7bd8a2ba19bd4a
Renan Georgio <43246444+RenanGeorgio@users.noreply.github.com>
2025-02-19 21:04:28 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export const viewport: Viewport = {
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://aura-tts-demo.deepgram.com"),
-  title: "Deepgram AI Agent",
-  description: `Deepgram's AI Agent Demo shows just how fast Speech-to-Text and Text-to-Speech can be.`,
+  title: "AI Agent",
+  description: `fast Speech-to-Text and Text-to-Speech.`,
   robots: {
     index: false,
     follow: false,
